66c4b2f5cca15c18f089b8221211abed1eab8c56
Apple <riyabulia12@gmail.com>
2022-11-01 18:48:28 +0530

    Create schema_based_on_diagram.sql

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,19 @@
+CREATE TABLE patients(
+   id INT GENERATED ALWAYS AS IDENTITY,
+   name VARCHAR(50),
+   date_of_birth DATE,
+   PRIMARY KEY(id));
+
+CREATE TABLE invoices(
+   id INT GENERATED ALWAYS AS IDENTITY,
+   total_amount DECIMAL,
+   generated_at TIMESTAMP,
+   payed_at TIMESTAMP,
+   medical_history_id INT,
+   PRIMARY KEY(id));
+
+CREATE TABLE medical_histories(
+   id SERIAL PRIMARY KEY,
+   admitted_at TIMESTAMP,
+   patient_id INT REFERENCES  patients(id),
+   status VARCHAR(50));
